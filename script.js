403f445144e7ace3384121b4a2e6585a676a008c
W2CH <modeb993@gmail.com>
2025-03-04 00:28:59 -0800

    Added a 3 input boxes and a submit button
    1. Defined a form with 7 elements
    2. Imported a javascript file to handle processing the book data
    
    I still need to define a table element to dynamically add book information.

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("add-book").addEventListener("click",addBook)
+})
+
+function addBook(){
+    var bookName = document.getElementById("book-name").value
+    var author = document.getElementById("book-author").value
+    var ISBM = document.getElementById("")
+}
