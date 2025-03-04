0c463321b29b1c058c62c98c7e9d3cb6b940f9a1
W2CH <modeb993@gmail.com>
2025-03-04 00:35:40 -0800

    Revert "Added a 3 input boxes and a submit button"
    This reverts commit 403f445144e7ace3384121b4a2e6585a676a008c.

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("add-book").addEventListener("click",addBook)
-})
-
-function addBook(){
-    var bookName = document.getElementById("book-name").value
-    var author = document.getElementById("book-author").value
-    var ISBM = document.getElementById("")
-}
