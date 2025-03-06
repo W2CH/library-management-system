25c3d9c4c5262a374020362dd933803ebff43552
W2CH <modeb993@gmail.com>
2025-03-06 11:38:57 -0800

    Almost done with the add book function
    1. Dynamically updates table each time we add a new book.
    2. Added a quantity column in the table.
    3. Created a map to store all unique books.
    4. Display error messages if user gave us a invalid duplicate book.
    
    I still need to create another column for each book with a edit event.
    That is probably going to take me a minute.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const books = new Map();
+
 document.addEventListener("DOMContentLoaded",function(){
 
     const form = document.getElementById("book-info")
@@ -7,19 +9,42 @@ document.addEventListener("DOMContentLoaded",function(){
 
         // Prevents submission to a server.
         event.preventDefault();
-        addBook();
 
-        // Remove the book info that was passed in after adding the book.
-        form.reset()
+        var bookWasAdded = addBook();
+        if(bookWasAdded){
+            // Remove the book info that was passed in after adding the book.
+            form.reset()
+        }
     })
 })
 
 function addBook(){
 
+    // This just removes the duplicate ISBN error message.
+    document.getElementById("add-status").innerHTML = ""
+
     // TODO: Get the values that the user passed in
     const bookTitle = document.getElementById("book-name").value;
     const author = document.getElementById("author").value;
     const ISBN = document.getElementById("ISBN").value;
+
+    // TODO Figure out if ISBN already exists and if does, check if its an invalid duplicate.
+    if(books.has(ISBN) && !duplicate(ISBN, bookTitle, author)){
+        console.log("book was not added!");
+       return false;
+    }
+
+    // TODO: Since this is a valid duplicate, just update quantity column.
+    if(books.has(ISBN)){
+
+        // Get the current quantity value of this book, cast it to int, update it, cast it back to string,
+        // and update the quantity column.
+        var currentQuantity = Number(document.getElementById(ISBN).cells[3].innerHTML)
+        currentQuantity++;
+        document.getElementById(ISBN).cells[3].innerHTML = currentQuantity.toString();
+        document.getElementById("add-status").innerHTML = "Book has been added!";
+        return true;
+    }
 
     // TODO Append the new book to the table
 
@@ -30,21 +55,53 @@ function addBook(){
     const newBookTitle = document.createElement("td");
     const newAuthor = document.createElement("td");
     const newISBN = document.createElement("td");
+    const bookQuantity = document.createElement("td");
 
     // Store book data to the table data elements
     newBookTitle.textContent = bookTitle;
     newAuthor.textContent = author;
     newISBN.textContent = ISBN;
-
+    bookQuantity.textContent = "1";
 
     // TODO: Set tr as the parent of td
     newRow.appendChild(newBookTitle);
     newRow.appendChild(newAuthor);
     newRow.appendChild(newISBN);
+    newRow.appendChild(bookQuantity);
 
     // TODO: Set tbody as the parent of tr
     const bookTable = document.getElementById("book-table");
     bookTable.appendChild(newRow);
 
-    console.log("Book has been added!")
+    // TODO: Give each new row a unique id using ISBN
+    newRow.id = ISBN;
+
+    // TODO: Store new book to our map.
+    books.set(ISBN, newRow);
+
+    document.getElementById("add-status").innerHTML = "Book has been added!";
+    return true;
+}
+
+function duplicate(ISBN, bookTitle, author){
+    // Grab the existing book's information with the existing ISBN
+    var existingBookTitle = books.get(ISBN).cells[0].innerHTML;
+    var existingAuthor = books.get(ISBN).cells[1].innerHTML;
+
+    // Check if the author and title of new book match the existing book
+    if(author !== existingAuthor && bookTitle !== existingBookTitle){
+        document.getElementById("add-status").innerHTML = "Title and Author do not match the existing book!";
+        return false
+    }
+    // Check if only the author name is wrong.
+    else if(author !== existingAuthor){
+        document.getElementById("add-status").innerHTML = "Author does not match the existing book!";
+        return false;
+    }
+    // Check if only the book title is wrong.
+    else if(bookTitle !== existingBookTitle){
+        document.getElementById("add-status").innerHTML = "Title does not match the existing book!";
+        return false;
+    }
+    return true;
 }
